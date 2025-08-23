c10fb770c368fa9f477115a84198a31dc3e8a5f9
ssantiago90 <salkin.santiago@gmail.com>
2025-08-23 15:45:59 -0300

    coder flex clase 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+
+function App() {  
+  return <div>      
+      <h1>Hola mundo</h1>    
+      <h2>Hola Coderhouse</h2>
+      <small>Esta es mi app de React</small>
+    </div>
+}
+
+export default App
